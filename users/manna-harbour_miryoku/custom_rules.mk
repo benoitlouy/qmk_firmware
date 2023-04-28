8d9dcdee8e67666b211ed6b5a56b9a5785e7f447
Benoit Louy <benoit.louy@disneystreaming.com>
2023-04-28 10:01:11 -0400

    enable led matrix

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -2,8 +2,8 @@
 # https://github.com/manna-harbour/miryoku
 
 MOUSEKEY_ENABLE = yes    # Mouse keys
-RGBLIGHT_ENABLE = yes # Enable WS2812 RGB underlight.
-RGB_MATRIX_ENABLE = no
+RGBLIGHT_ENABLE = no # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE = yes
 OLED_ENABLE     = yes
 OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
